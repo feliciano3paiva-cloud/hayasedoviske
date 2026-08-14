5d924efaf8e407afdec3e08def3752e046768bee
levandoviske <feliciano3paiva@gmail.com>
2026-08-14 02:43:55 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,12 +1,22 @@
-export default new class TesteLegendas {
-  async single(query) {
-    return [
-      {
-        url: "https://raw.githubusercontent.com/feliciano3paiva-cloud/hayasedoviske/main/test.srt",
-        language: "BR"
-      }
-    ]
+export default new class Teste {
+  async single({ titles, episode }) {
+    return [{
+      title: `TESTE EXTENSÃO - ${titles?.[0] || "Anime"} - EP ${episode ?? "?"}`,
+      link: "magnet:?xt=urn:btih:0000000000000000000000000000000000000000",
+      hash: "0000000000000000000000000000000000000000",
+      seeders: 999,
+      leechers: 0,
+      downloads: 999,
+      size: 123456789,
+      date: new Date(),
+      verified: true,
+      type: "alt",
+      accuracy: "high"
+    }]
   }
+
+  batch = this.single
+  movie = this.single
 
   async test() {
     return true
