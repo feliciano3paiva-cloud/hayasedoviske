c633bae33327d66c3456706e8d27d68aaf25a344
levandoviske <feliciano3paiva@gmail.com>
2026-08-14 02:39:03 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,22 +1,12 @@
-export default new class Teste {
-  async single({ titles, episode }) {
-    return [{
-      title: `TESTE HAYASE - ${titles?.[0] || "Anime"} - EP ${episode ?? "?"}`,
-      link: "magnet:?xt=urn:btih:0000000000000000000000000000000000000000",
-      hash: "0000000000000000000000000000000000000000",
-      seeders: 999,
-      leechers: 0,
-      downloads: 999,
-      size: 123456789,
-      date: new Date(),
-      verified: true,
-      type: "alt",
-      accuracy: "high"
-    }]
+export default new class TesteLegendas {
+  async single(query) {
+    return [
+      {
+        url: "https://raw.githubusercontent.com/feliciano3paiva-cloud/hayasedoviske/main/test.srt",
+        language: "BR"
+      }
+    ]
   }
-
-  batch = this.single
-  movie = this.single
 
   async test() {
     return true
