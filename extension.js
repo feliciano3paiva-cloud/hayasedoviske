01ea603fa7bfc099f3cbe03e417a1b66b61cbc06
levandoviske <feliciano3paiva@gmail.com>
2026-08-14 02:37:19 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,17 +1,17 @@
 export default new class Teste {
-  async single(query) {
+  async single({ titles, episode }) {
     return [{
-      title: 'TESTE HAYASE - EXTENSÃO FUNCIONANDO',
-      link: 'magnet:?xt=urn:btih:0000000000000000000000000000000000000000',
-      hash: '0000000000000000000000000000000000000000',
+      title: `TESTE HAYASE - ${titles?.[0] || "Anime"} - EP ${episode ?? "?"}`,
+      link: "magnet:?xt=urn:btih:0000000000000000000000000000000000000000",
+      hash: "0000000000000000000000000000000000000000",
       seeders: 999,
       leechers: 0,
       downloads: 999,
       size: 123456789,
       date: new Date(),
       verified: true,
-      type: 'alt',
-      accuracy: 'high'
+      type: "alt",
+      accuracy: "high"
     }]
   }
 
