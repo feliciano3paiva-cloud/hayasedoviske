346b799795a2ab87212497de172abb3ce0dd9f8d
levandoviske <feliciano3paiva@gmail.com>
2026-08-14 02:34:21 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,6 +1,18 @@
-export default new class OpenSubtitles {
+export default new class Teste {
   async single(query) {
-    return []
+    return [{
+      title: 'TESTE HAYASE - EXTENSÃO FUNCIONANDO',
+      link: 'magnet:?xt=urn:btih:0000000000000000000000000000000000000000',
+      hash: '0000000000000000000000000000000000000000',
+      seeders: 999,
+      leechers: 0,
+      downloads: 999,
+      size: 123456789,
+      date: new Date(),
+      verified: true,
+      type: 'alt',
+      accuracy: 'high'
+    }]
   }
 
   batch = this.single
